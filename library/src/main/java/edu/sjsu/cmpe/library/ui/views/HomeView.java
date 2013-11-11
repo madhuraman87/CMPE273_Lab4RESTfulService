800ceb78a3fd4c603efee70e8c352ad21f781d69
Madhuvanthi Ramanujam <madhuraman87@gmail.com>
2013-11-10 22:07:00 -0800

    imported list package

--- library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
+++ library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
@@ -1,7 +1,7 @@
 package edu.sjsu.cmpe.library.ui.views;
 
 import com.yammer.dropwizard.views.View;
-
+import java.util.List;
 import edu.sjsu.cmpe.library.domain.Book;
 
 public class HomeView extends View 
